d03b0513ab42f6babcb1f490b2dc9300836e8a4d
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-11-21 15:06:44 -0600

    Updates

---
+++ precache-manifest.fc5420040855e0fc2fa1ac5cd30b98ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a320d9e2fdf42cb979efd17c342edcb9",
+    "url": "/RosaIsela31/finderfuel-MockHachathon/index.html"
+  },
+  {
+    "revision": "0d63057d1e6f6405fb4c",
+    "url": "/RosaIsela31/finderfuel-MockHachathon/static/css/main.8e1a194f.chunk.css"
+  },
+  {
+    "revision": "8f0c15f6a6f3619642bb",
+    "url": "/RosaIsela31/finderfuel-MockHachathon/static/js/2.d5e69e67.chunk.js"
+  },
+  {
+    "revision": "0d63057d1e6f6405fb4c",
+    "url": "/RosaIsela31/finderfuel-MockHachathon/static/js/main.0942c8d6.chunk.js"
+  },
+  {
+    "revision": "5c56da4f411e7a29b214",
+    "url": "/RosaIsela31/finderfuel-MockHachathon/static/js/runtime-main.358e8f9f.js"
+  }
+]);
