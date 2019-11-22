e25c38ad27afb45516e328edf5cc049fa6914135
OyukiCV <oyukivel@hotmail.com>
2019-11-22 15:21:20 -0600

    update second view's map with all pointers

---
+++ src/views/secondMap/secondMap.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+
+import CurrentLocation from '../../components/map/CurrentLocation';
+import NameList from '../../views/NameList';
+
+import * as gasData from '../../components/map/locations.json';
+
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  render() {
+    return (
+      <CurrentLocation
+        centerAroundCurrentLocation
+        google={this.props.google}
+       
+      >
+        {gasData.gasdata.map((station)=>(
+    
+        
+       
+          <Marker  key={station.location} 
+          position={{
+            lat:station.location.y,
+            lng:station.location.x,
+          }} onClick={this.onMarkerClick} name={'current location'} />
+        
+          ))}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAebXFF8h-t7eN5aft9vC0qpndBTKr5tfY'
+})(MapContainer);
